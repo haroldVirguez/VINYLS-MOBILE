800d217e6bba28fb685b9f48113d2942940d7a6a
Jonatan Hernandez <je.hernandezr@uniandes.edu.co>
2025-11-16 17:48:17 -0500

    advance

--- app/src/main/java/com/team3/vinyls/data/services/AlbumsService.kt
+++ app/src/main/java/com/team3/vinyls/data/services/AlbumsService.kt
@@ -1,8 +1,11 @@
 package com.team3.vinyls.data.services
 
 import com.team3.vinyls.core.network.ApiConstants
+import com.team3.vinyls.data.models.AlbumCreateDto
 import com.team3.vinyls.data.models.AlbumDto
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface AlbumsService {
@@ -11,4 +14,7 @@ interface AlbumsService {
 
     @GET("${ApiConstants.ALBUMS_ENDPOINT}/{id}")
     suspend fun getAlbumDetail(@Path("id") albumId: Int): AlbumDto
+
+    @POST(ApiConstants.ALBUMS_ENDPOINT)
+    suspend fun createAlbum(@Body albumBody: AlbumCreateDto): AlbumDto
 }
