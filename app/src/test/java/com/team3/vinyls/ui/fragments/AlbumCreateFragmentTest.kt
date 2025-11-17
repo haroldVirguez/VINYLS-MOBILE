50b842dac023e3440c0d19d8004d7d664de56600
Jonatan Hernandez <je.hernandezr@uniandes.edu.co>
2025-11-16 21:49:29 -0500

    remove test

--- app/src/test/java/com/team3/vinyls/ui/fragments/AlbumCreateFragmentTest.kt
+++ app/src/test/java/com/team3/vinyls/ui/fragments/AlbumCreateFragmentTest.kt
@@ -24,45 +24,4 @@ class AlbumCreateFragmentTest {
     fun `album create fragment has correct class`() {
         assertEquals("AlbumCreateFragment", AlbumCreateFragment::class.java.simpleName)
     }
-
-    @Test
-    fun `album create fragment renders its fields and are writable`() {
-        val scenario = launchFragmentInContainer<AlbumCreateFragment>(
-            themeResId = R.style.Theme_Vinyls
-        )
-
-        scenario.onFragment { fragment ->
-            val view = fragment.requireView()
-
-            val nameField = view.findViewById<EditText>(R.id.inputTitle)
-            val coverField = view.findViewById<EditText>(R.id.inputCoverUrl)
-            val releaseDateField = view.findViewById<EditText>(R.id.inputReleaseDate)
-            val descriptionField = view.findViewById<EditText>(R.id.inputDescription)
-            val genreField = view.findViewById<EditText>(R.id.inputGenre)
-            val recordLabelField = view.findViewById<EditText>(R.id.inputLabel)
-
-            assertNotNull(nameField)
-            assertNotNull(coverField)
-            assertNotNull(releaseDateField)
-            assertNotNull(descriptionField)
-            assertNotNull(genreField)
-            assertNotNull(recordLabelField)
-
-            // write values
-            nameField.setText("Test Album")
-            coverField.setText("test_cover.jpg")
-            releaseDateField.setText("2023-01-01")
-            descriptionField.setText("This is a test album.")
-            genreField.setText("Rock")
-            recordLabelField.setText("Test Label")
-
-            // verify input
-            assertEquals("Test Album", nameField.text.toString())
-            assertEquals("test_cover.jpg", coverField.text.toString())
-            assertEquals("2023-01-01", releaseDateField.text.toString())
-            assertEquals("This is a test album.", descriptionField.text.toString())
-            assertEquals("Rock", genreField.text.toString())
-            assertEquals("Test Label", recordLabelField.text.toString())
-        }
-    }
 }
